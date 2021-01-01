0eb9ebc969b6769ffbe6fb5a4bb9bb71f0538afa
Rashad Moawad <rashad.moawad@crossover.com>
2021-01-01 16:18:13 +0200

    Auth unit test coverage

---
+++ node/src/test/auth/authorize.spec.js
@@ -0,0 +1,30 @@
+﻿const authorize = require('../../app/auth/authorize');
+jest.mock('../../app/database/db', () => {
+    return {
+        User: {
+            findByPk: (id) => {
+                if (id === -1) {
+                  return undefined;
+                }
+
+                return {
+                    id: 1, username: 'test',
+                    get: () => { username: 'test' }
+                }
+            }
+        }
+    };
+});
+
+describe("Authorize Service functions", () => {
+    it("authorize()", async () => {
+        // Arrange
+        const next = jest.fn(() => {});
+
+        // Act
+        await authorize()[1]({ user: {sub: 1} }, {}, next)
+
+        //Assert
+        expect(next).toHaveBeenCalled();
+    });
+});
